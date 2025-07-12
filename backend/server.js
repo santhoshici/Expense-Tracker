3ad0fa8831eef5febff5415c4a433a23d0571e74
Santhoshici <santhosh05042006@gmail.com>
2025-07-12 14:28:57 +0530

    Login, SignUp API Commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+const app = express();
+const authroutes = require("./routes/authRoutes");
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/v1/auth", authroutes);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads"))); 
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
